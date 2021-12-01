ef55ca239382091093ece13968bf3456eedf6692
nekstas <nek.stas.other@yandex.ru>
2021-12-01 21:05:11 +0300

    Стили клеток.

--- core/other/cell_s.py
+++ core/other/cell_s.py
@@ -5,8 +5,13 @@
 class CellS:
     FONT_SIZES = [0, 28, 20, 16]
 
-    BASE = '''border-radius: 20px;
-color: #fff;
-text-transform: uppercase;'''
-    ENABLED = '''background-color: #8a8eff;'''
-    DISABLED = '''background-color: #d3514b;'''
+    BASE = '''* {
+    border-radius: 20px;
+    color: #fff;
+    text-transform: uppercase;'''
+    ENABLED = '''background-color: #8a8eff;
+}
+*:hover {
+    background: #999cff;
+}'''
+    DISABLED = '''background-color: #d3514b; }'''
